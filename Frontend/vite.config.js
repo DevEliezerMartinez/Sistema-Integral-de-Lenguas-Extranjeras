92cd91dfa51e9c8af47af13ffd691192e85824c6
Eliezer Solano Martinez <83482063+DevEliezerMartinez@users.noreply.github.com>
2024-11-01 09:07:48 -0600

    include credential

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Asegúrate de que la base esté configurada correctamente
+
 })
